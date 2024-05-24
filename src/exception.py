46dea66caf3e7cb890265740db7d2e31ed18965e
shashi <sshashirns@gmail.com>
2024-05-24 11:35:40 +0530

    exeption and logger

---
+++ src/exception.py
@@ -0,0 +1,17 @@
+import sys
+from logger import logging
+
+def error_message(error, error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_str = f"Error occurred in file: {file_name}, line no: {exc_tb.tb_lineno}, error: {str(error)}"
+    # logging.info(error_str)
+    return error_str
+
+class CustomException(Exception):
+    def __init__(self, error_msg, error_details:sys):
+        super().__init__(error_msg)
+        self.error_msg = error_message(error_msg, error_details)
+    
+    def __str__(self):
+        return self.error_msg
